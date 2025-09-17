1f341fcb5224e5468497a96ff933f1069bbc63cd
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-09-17 13:14:51 +0530

    Create bottomnavbar.dart

---
+++ lib/screens/bottomnavbar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:skillbridge/screens/homescreen.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int selectedIndex = 0;
+  List screenList = [
+    const HomeScreen(),
+    const HomeScreen(),
+    const HomeScreen(),
+    const HomeScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screenList.elementAt(selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.white.withOpacity(0.5),
+        selectedItemColor: Color(0xFF084C5C),
+        selectedLabelStyle: TextStyle(
+          color: Color(0xFF084C5C),
+        ),
+        selectedIconTheme: IconThemeData(
+          color: Color(0xFF084C5C),
+        ),
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        type: BottomNavigationBarType.fixed,
+        currentIndex: selectedIndex,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: "Schedules"),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "Chats"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ]),
+    );
+  }
+}
