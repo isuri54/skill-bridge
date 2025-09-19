98dce104cfe38bbbaf99a753bcabff1fc563766c
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-09-19 23:00:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:skillbridge/screens/loginscreen.dart';
 import 'package:skillbridge/screens/signupscreen.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const SignupScreen(),
+      home: const LoginScreen(),
     );
   }
 }
