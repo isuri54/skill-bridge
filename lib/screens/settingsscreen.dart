77c3914a6edb1e8f04ef13008186a20d23f777c1
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-10-06 11:01:11 +0530

    Update settingsscreen.dart

--- lib/screens/settingsscreen.dart
+++ lib/screens/settingsscreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:skillbridge/screens/loginscreen.dart';
 import 'package:skillbridge/screens/signupscreen.dart';
 import 'profilescreen.dart';
 
@@ -81,7 +82,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
   Future<void> _logout() async {
     try {
       await FirebaseAuth.instance.signOut();
-      Get.offAll(() => const SignupScreen());
+      Get.offAll(() => const LoginScreen());
       print('User logged out');
     } catch (e) {
       print('Error logging out: $e');
